abe36964d9d4956c95f105679d813b03275078b9
Folaranmi Jesutofunmi <folaranmijesutofunmi@gmail.com>
2021-12-25 06:15:28 +0100

    Updated UserTest

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -20,6 +20,20 @@ class UserTest extends TestCase
         $statement = DB::$con->prepare("TRUNCATE TABLE `users`");
         $statement->execute();
     }
+    
+    public function create_user(array $data)
+    {
+        $create_user = $this->client->post('/users', ['form_params' => $data]);
+        $user = json_decode($create_user->getBody()->getContents(), true);
+        return $user;
+    }
+
+    public function login(array $credentials)
+    {
+        $user = $this->client->post('/login', ['form_params' => $credentials]);
+        $user = json_decode($user->getBody()->getContents(), true);
+        return $user["token"];
+    }      
 
     public function test_user_returns_correct_data_on_create()
     { 
@@ -150,5 +164,66 @@ class UserTest extends TestCase
         $this->assertFalse(array_key_exists("password", json_decode($user->getBody()->getContents(), true)));
     }
 
-    
+    public function test_that_you_need_to_login_before_getting_user()
+    {
+        //create user
+        $data = 
+        [
+            "name" => "John Doe",
+            "email" => "johndoe@gmail.com",
+            "phonenumber" => "0809009090",
+            "password" => "password"
+        ]; 
+
+        $create_user = $this->client->post('/users', ['form_params' => $data]);
+
+        //get user
+        $user_id = json_decode($create_user->getBody()->getContents(), true)["id"];//user id
+
+        $get_user = $this->client->get("/users/".$user_id,  ['http_errors' => false]);
+        $user = json_decode($get_user->getBody()->getContents(), true);
+
+        $this->assertEquals("401", $get_user->getStatusCode());
+    }
+
+    public function test_that_we_can_get_user()
+    {
+        //create user
+        $data = 
+        [
+            "name" => "John Doe",
+            "email" => "johndoe@gmail.com",
+            "phonenumber" => "0809009090",
+            "password" => "password"
+        ]; 
+
+        $created_user = $this->create_user($data);
+        
+        //login
+        $token = $this->login(["email" => $data["email"], "password" => $data["password"]]);
+
+        //get user  
+        $header = 
+        [
+            'headers' => ["Authorization" => "Bearer ".$token]
+        ];
+
+        $get_user = $this->client->get("/users/".$created_user["id"], $header);
+
+        $user = json_decode($get_user->getBody()->getContents(), true);
+
+        $this->assertTrue(array_key_exists("id", $user));
+        $this->assertTrue(array_key_exists("name", $user));
+        $this->assertTrue(array_key_exists("created_at", $user));
+        $this->assertTrue(array_key_exists("updated_at", $user));
+
+        $this->assertEquals("John Doe", ($user["name"]));
+        $this->assertEquals("johndoe@gmail.com", ($user["email"]));
+        $this->assertEquals("0809009090", ($user["phonenumber"]));
+        $this->assertEquals("200", $get_user->getStatusCode());
+
+    }
+
+    //test_that_you_can_only_update_your_own_self
+    //test_that_you_can_update_user
 }
